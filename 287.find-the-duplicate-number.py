5943238c7bdd87307cc9bc5f2eff68c5b3149f5d
samarthpatel1289 <samarth.patel1289@gmail.com>
2024-03-02 09:15:49 -0600

    Find Duplicate Number without extra space. 16.33

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -6,6 +6,15 @@
 
 # @lc code=start
 """
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+"""
+
+"""
+Intuition
 SC : O(1)
 
 TC : O(n^2)
@@ -20,8 +29,21 @@ TC : O(nlogn) + O(n) => O(2n log n) => o(nlogn)
         - if similar return num
         - else + 1
 """
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        if n == 1:
+            return nums
+
+        i = 0
+        while i + 1 < n:
+            if nums[i] == nums[i+1]:
+                return nums[i]
+            i += 1
 
 """
+Dry Run
 [1,3,4,2,2]
 
 slow 0  1   2   4
@@ -37,19 +59,6 @@ slow2   0   4   2   3   4   2
  0  1. 2. 3. 4. 5  6
 [2, 3, 1, 3, 4, 5, 6]
 """
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        if n == 1:
-            return nums
-
-        i = 0
-        while i + 1 < n:
-            if nums[i] == nums[i+1]:
-                return nums[i]
-            i += 1
         
 # @lc code=end
 
